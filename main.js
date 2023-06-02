af109a8c46c440636bdaee296b7709d80080def4
Rites1 <103243296+Rites1@users.noreply.github.com>
2023-06-02 12:41:06 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,18 +7,22 @@ function setup(){
     canvas.position(110, 250);
     video = createCapture(VIDEO);
     video.hide();
-    tint_color = "";
 }
 
 function draw(){
-    image(video, 0,0,640, 480);
-    tint(tint_color);
-}
-
-function filter_tint(){
-    tint_color = document.getElementById("color_name").value;
+    image(video, 130, 100,400, 300);
+    fill("blue");
+    stroke("green");
+    circle(50, 50, 80);
+    circle(50, 430, 80);
+    circle(590, 50, 80);
+    circle(590, 430, 80);
+    rect(90, 40, 460, 20);
+    rect(90, 420, 460, 20);
+    rect(580, 90, 20, 300);
+    rect(40, 90, 20, 300);
 }
 
 function take_snapshot(){
-    save("student.png");
+    save("snapshot.png");
 }
